6a8bca2ef377447f7b6bb8381f32387210674f32
A01422807 <43050815+amatsonkali@users.noreply.github.com>
2022-05-02 15:42:56 -0500

    Bassic OFW w/ http request

--- pallets/nbv-storage/src/lib.rs
+++ pallets/nbv-storage/src/lib.rs
@@ -36,15 +36,30 @@ pub mod crypto {
 #[frame_support::pallet]
 pub mod pallet {
 use frame_support::pallet_prelude::*;
-	use frame_system::{pallet_prelude::*, offchain::CreateSignedTransaction};
-
+	use frame_system::{pallet_prelude::*, offchain::{
+		AppCrypto, CreateSignedTransaction, SendSignedTransaction, SendUnsignedTransaction,
+		SignedPayload, Signer, SigningTypes, SubmitTransaction,},
+	};
+	use sp_runtime::{
+		offchain::{
+			http,
+			storage::{MutateStorageError, StorageRetrievalError, StorageValueRef},
+			Duration,
+		},
+		traits::Zero,
+		transaction_validity::{InvalidTransaction, TransactionValidity, ValidTransaction},
+		RuntimeDebug,
+	};
+	use sp_runtime::sp_std::str;
+	use sp_runtime::sp_std::vec::Vec;
 	use frame_support::{
 		BoundedVec,
 		transactional,
 		sp_io::hashing::blake2_256,
 	};
 	use scale_info::prelude::boxed::Box;
-
+	use lite_json::json::JsonValue;
+	
 	//use scale_info::TypeInfo;
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
@@ -141,6 +156,7 @@ use frame_support::pallet_prelude::*;
 		fn offchain_worker(block_number: T::BlockNumber) {
 			log::info!("Hello from pallet-ocw. Block number {:?}",block_number);
 			// The entry point of your code called by off-chain worker
+			log::info!("{:?}",Self::fetch_price() );
 		}
 		// ...
 	}
@@ -312,6 +328,81 @@ use frame_support::pallet_prelude::*;
 			// new xpub registry: available 
 			XpubStatus::Free
 		}
+
+		fn fetch_price() -> Result<u32, http::Error> {
+			// We want to keep the offchain worker execution time reasonable, so we set a hard-coded
+			// deadline to 2s to complete the external call.
+			// You can also wait idefinitely for the response, however you may still get a timeout
+			// coming from the host machine.
+			let deadline = sp_io::offchain::timestamp().add(Duration::from_millis(2_000));
+			// Initiate an external HTTP GET request.
+			// This is using high-level wrappers from `sp_runtime`, for the low-level calls that
+			// you can find in `sp_io`. The API is trying to be similar to `reqwest`, but
+			// since we are running in a custom WASM execution environment we can't simply
+			// import the library here.
+			let request =
+				http::Request::get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD");
+			// We set the deadline for sending of the request, note that awaiting response can
+			// have a separate deadline. Next we send the request, before that it's also possible
+			// to alter request headers or stream body content in case of non-GET requests.
+			let pending = request.deadline(deadline).send().map_err(|_| http::Error::IoError)?;
+	
+			// The request is already being processed by the host, we are free to do anything
+			// else in the worker (we can send multiple concurrent requests too).
+			// At some point however we probably want to check the response though,
+			// so we can block current thread and wait for it to finish.
+			// Note that since the request is being driven by the host, we don't have to wait
+			// for the request to have it complete, we will just not read the response.
+			let response = pending.try_wait(deadline).map_err(|_| http::Error::DeadlineReached)??;
+			// Let's check the status code before we proceed to reading the response.
+			if response.code != 200 {
+				log::warn!("Unexpected status code: {}", response.code);
+				return Err(http::Error::Unknown)
+			}
+	
+			// Next we want to fully read the response body and collect it to a vector of bytes.
+			// Note that the return object allows you to read the body in chunks as well
+			// with a way to control the deadline.
+			let body = response.body().collect::<Vec<u8>>();
+	
+			// Create a str slice from the body.
+			let body_str = str::from_utf8(&body).map_err(|_| {
+				log::warn!("No UTF8 body");
+				http::Error::Unknown
+			})?;
+	
+			let price = match Self::parse_price(body_str) {
+				Some(price) => Ok(price),
+				None => {
+					log::warn!("Unable to extract price from the response: {:?}", body_str);
+					Err(http::Error::Unknown)
+				},
+			}?;
+	
+			log::warn!("Got price: {} cents", price);
+	
+			Ok(price)
+		}
+
+		/// Parse the price from the given JSON string using `lite-json`.
+	///
+	/// Returns `None` when parsing failed or `Some(price in cents)` when parsing is successful.
+	fn parse_price(price_str: &str) -> Option<u32> {
+		let val = lite_json::parse_json(price_str);
+		let price = match val.ok()? {
+			JsonValue::Object(obj) => {
+				let (_, v) = obj.into_iter().find(|(k, _)| k.iter().copied().eq("USD".chars()))?;
+				match v {
+					JsonValue::Number(number) => number,
+					_ => return None,
+				}
+			},
+			_ => return None,
+		};
+
+		let exp = price.fraction_length.saturating_sub(2);
+		Some(price.integer as u32 * 100 + (price.fraction / 10_u64.pow(exp)) as u32)
+	}
     }
 
 
