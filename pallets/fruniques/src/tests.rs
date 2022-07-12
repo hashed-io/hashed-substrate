32a95e429fcafdf2f1b2f6683c7992f7df042169
Erick Casanova <desneruda@gmail.com>
2022-07-11 21:10:57 -0500

    format document

--- pallets/fruniques/src/tests.rs
+++ pallets/fruniques/src/tests.rs
@@ -1,6 +1,6 @@
-use crate::{Error, mock::*};
+use crate::{mock::*, Error};
 
-use frame_support::{assert_ok, assert_err, assert_noop};
+use frame_support::{assert_err, assert_noop, assert_ok};
 use sp_runtime::Permill;
 
 pub struct ExtBuilder;
@@ -37,27 +37,27 @@ impl ExtBuilder {
 fn create_frunique_works() {
 	// Create a frunique
 	ExtBuilder::default().build().execute_with(|| {
-		assert_ok!(Fruniques::create(Origin::signed(1), 1, 0,Some(Permill::from_percent(50)) ,1));
+		assert_ok!(Fruniques::create(Origin::signed(1), 1, 0, Some(Permill::from_percent(50)), 1));
 	});
 }
 
 // this test is failing for some reason...
 /*---- tests::spawn_extrinsic_works stdout ----
 thread 'tests::spawn_extrinsic_works' panicked at 'Expected Ok(_). Got Err(
-    Module(
-        ModuleError {
-            index: 1,
-            error: [
-                1,
-                0,
-                0,
-                0,
-            ],
-            message: Some(
-                "UnknownCollection",
-            ),
-        },
-    ),
+		Module(
+				ModuleError {
+						index: 1,
+						error: [
+								1,
+								0,
+								0,
+								0,
+						],
+						message: Some(
+								"UnknownCollection",
+						),
+				},
+		),
 )', pallets/fruniques/src/tests.rs:41:9
 note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
 */
@@ -65,9 +65,9 @@ note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
 fn spawn_extrinsic_works() {
 	ExtBuilder::default().build().execute_with(|| {
 		//assert_ok!(Fruniques::create(Origin::signed(1), 1,255,Some(100), 1));
-		assert_ok!(Fruniques::spawn(Origin::signed(1), 1, 255,true,Permill::from_float(20.525),1) );
+		assert_ok!(Fruniques::spawn(Origin::signed(1), 1, 255, true, Permill::from_float(20.525), 1));
 		//Fruniques::spawn(Origin::signed(1),1,255,true,Permill::from_float(20.525),1 );
-		assert_ok!(Fruniques::spawn(Origin::signed(1), 1, 1, true,Permill::from_float(20.525),1));
+		assert_ok!(Fruniques::spawn(Origin::signed(1), 1, 1, true, Permill::from_float(20.525), 1));
 		assert_ok!(Fruniques::instance_exists(Origin::signed(1), 1, 1));
 	});
 }
@@ -75,15 +75,18 @@ fn spawn_extrinsic_works() {
 #[test]
 fn set_attributes_fail_case() {
 	ExtBuilder::default().build().execute_with(|| {
-		assert_ok!(Fruniques::create(Origin::signed(1), 0, 0,Some(Permill::from_percent(50)) ,1));
-		assert_noop!(Fruniques::set_attributes(Origin::signed(1), 0, 0, vec![]), Error::<Test>::AttributesEmpty);
+		assert_ok!(Fruniques::create(Origin::signed(1), 0, 0, Some(Permill::from_percent(50)), 1));
+		assert_noop!(
+			Fruniques::set_attributes(Origin::signed(1), 0, 0, vec![]),
+			Error::<Test>::AttributesEmpty
+		);
 	});
 }
 
 #[test]
 fn set_attributes() {
 	ExtBuilder::default().build().execute_with(|| {
-		assert_ok!(Fruniques::create(Origin::signed(1), 0, 0,Some(Permill::from_percent(50)) ,1));
+		assert_ok!(Fruniques::create(Origin::signed(1), 0, 0, Some(Permill::from_percent(50)), 1));
 		assert_ok!(Fruniques::set_attributes(Origin::signed(1), 0, 0, vec![(bvec![0], bvec![0])]));
 	});
 }
