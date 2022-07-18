e0599bae6b9d159d7632bca5dc55b770a1e42209
amatsonkali <amatsonkali@outlook.com>
2022-07-18 09:58:35 -0500

    dockerfile and start_node changes in order to have a working mainnet image

---
+++ scripts/start_node.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+cargo build --release
+
+node_args=(
+    --base-path ./hashed-chaos-data/ 
+    --ws-external 
+    --rpc-external 
+    --rpc-cors all 
+    --rpc-methods unsafe 
+    --validator 
+    --node-key ${NODEKEY}
+)
+
+if [[ ${ISMAINNET} = true ]]; then
+    echo "Mainnet deployment detected"
+    # In Docker the image abhashed/hashednet:mainnet, chaosSpec.json was changed to chaos2.json 
+    chain_spec="--chain chaosSpec.json"
+    node_args+=(--bootnodes /ip4/206.221.189.10/tcp/30335/p2p/12D3KooWQxwQyQ3BaCs5tweoTmHNWHbpHePZt6P9SscBps1FWsUc)
+else
+echo "MD5 deployment detected"
+    chain_spec="--chain md5Spec.json"
+fi
+node_args+=($chain_spec)
+
+echo "Inserting keys..."
+
+./target/release/hashed key insert --base-path ./hashed-chaos-data $chain_spec --scheme sr25519 --suri "${MNEMO}" --key-type aura
+
+./target/release/hashed key insert --base-path ./hashed-chaos-data $chain_spec --scheme ed25519 --suri "${MNEMO}" --key-type gran
+
+
+./target/release/hashed "${node_args[@]}"
