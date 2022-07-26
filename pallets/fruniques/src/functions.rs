b8010197d11a637f96626a1ca719882a6ec8e0d4
Erick Casanova <desneruda@gmail.com>
2022-07-26 17:01:27 -0500

    add some helper functions - burn - freeze - accountID to Lookup source

--- pallets/fruniques/src/functions.rs
+++ pallets/fruniques/src/functions.rs
@@ -27,6 +27,13 @@ impl<T: Config> Pallet<T> {
 		}
 		s
 	}
+
+	pub fn account_id_to_lookup_source(
+		account_id: &T::AccountId,
+	) -> <T::Lookup as sp_runtime::traits::StaticLookup>::Source {
+		<T::Lookup as sp_runtime::traits::StaticLookup>::unlookup(account_id.clone())
+	}
+
 	/// Helper function for printing purposes
 	pub fn get_nft_attribute(
 		class_id: &T::CollectionId,
@@ -44,6 +51,62 @@ impl<T: Config> Pallet<T> {
 		pallet_uniques::Pallet::<T>::owner(*class_id, *instance_id)
 	}
 
+	pub fn set_attribute(
+		origin: OriginFor<T>,
+		class_id: &T::CollectionId,
+		instance_id: T::ItemId,
+		key: BoundedVec<u8, T::KeyLimit>,
+		value: BoundedVec<u8, T::ValueLimit>,
+	) -> DispatchResult {
+		pallet_uniques::Pallet::<T>::set_attribute(
+			origin,
+			*class_id,
+			Some(instance_id),
+			key,
+			value,
+		)?;
+		Ok(())
+	}
+
+	pub fn mint(
+		origin: OriginFor<T>,
+		class_id: &T::CollectionId,
+		instance_id: T::ItemId,
+		owner: <T::Lookup as sp_runtime::traits::StaticLookup>::Source,
+	) -> DispatchResult {
+		pallet_uniques::Pallet::<T>::mint(origin, *class_id, instance_id, owner)?;
+		Ok(())
+	}
+
+	pub fn freeze(
+		origin: OriginFor<T>,
+		class_id: &T::CollectionId,
+		instance_id: T::ItemId,
+	) -> DispatchResult {
+		pallet_uniques::Pallet::<T>::freeze(origin, *class_id, instance_id)?;
+		Ok(())
+	}
+
+	// TODO: add a function to get the owner of an instance
+	// TODO: add a function to burn an instance
+	pub fn burn(
+		origin: OriginFor<T>,
+		class_id: &T::CollectionId,
+		instance_id: T::ItemId,
+	) -> DispatchResult {
+		let admin = Self::admin_of(class_id, &instance_id);
+		ensure!(admin.is_some(), "Instance is not owned by anyone");
+
+		pallet_uniques::Pallet::<T>::burn(
+			origin,
+			*class_id,
+			instance_id,
+			Some(Self::account_id_to_lookup_source(&admin.unwrap())),
+		)?;
+		Ok(())
+	}
+
+	// TODO: add a function to transfer an instance
 	pub fn do_create(
 		origin: OriginFor<T>,
 		class_id: T::CollectionId,
@@ -73,33 +136,6 @@ impl<T: Config> Pallet<T> {
 	}
 
 	pub fn do_spawn() -> DispatchResult {
-		Ok(())
-	}
-
-	pub fn set_attribute(
-		origin: OriginFor<T>,
-		class_id: &T::CollectionId,
-		instance_id: T::ItemId,
-		key: BoundedVec<u8, T::KeyLimit>,
-		value: BoundedVec<u8, T::ValueLimit>,
-	) -> DispatchResult {
-		pallet_uniques::Pallet::<T>::set_attribute(
-			origin,
-			*class_id,
-			Some(instance_id),
-			key,
-			value,
-		)?;
-		Ok(())
-	}
-
-	pub fn mint(
-		origin: OriginFor<T>,
-		class_id: &T::CollectionId,
-		instance_id: T::ItemId,
-		owner: <T::Lookup as sp_runtime::traits::StaticLookup>::Source,
-	) -> DispatchResult {
-		pallet_uniques::Pallet::<T>::mint(origin, *class_id, instance_id, owner)?;
 		Ok(())
 	}
 }
