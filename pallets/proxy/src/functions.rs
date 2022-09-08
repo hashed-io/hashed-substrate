b5a0b0e726ba4dba4a356ab0a1eff8731efd29ce
didiermis <didier.mis.may@gmail.com>
2022-09-08 17:31:16 -0500

    create initial functios.rs

---
+++ pallets/proxy/src/functions.rs
@@ -0,0 +1,17 @@
+use super::*;
+use frame_support::{pallet_prelude::*};
+//use frame_system::pallet_prelude::*;
+//use frame_support::sp_io::hashing::blake2_256;
+
+
+use crate::types::*;
+
+impl<T: Config> Pallet<T> {
+
+
+
+
+
+
+
+}
