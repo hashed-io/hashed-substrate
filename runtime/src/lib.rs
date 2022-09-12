65cfec333df980e9d9fd7ae96b794e5cd820cd53
didiermis <didier.mis.may@gmail.com>
2022-09-12 18:53:09 -0500

    add constant CIDMaxLen

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -553,6 +553,7 @@ parameter_types! {
 	pub const MaxDocuments:u32 = 5;
 	pub const MaxAccountsPerTransaction:u32 = 5;
 	pub const MaxProjectsPerUser:u32 = 10;
+	pub const CIDMaxLen:u32 = 64;
 }
 impl pallet_proxy::Config for Runtime {
 	type Event = Event;
@@ -562,6 +563,7 @@ impl pallet_proxy::Config for Runtime {
 	type MaxAccountsPerTransaction = MaxAccountsPerTransaction;
 	type MaxProjectsPerUser = MaxProjectsPerUser;
 	type MaxChildrens = MaxChildrens;
+	type CIDMaxLen = CIDMaxLen;
 
 }
 
