f32f5032de5979f7a01470b12c74e271488df379
amatsonkali <amatsonkali@users.noreply.github.com>
2022-10-10 18:12:18 +0000

    deploy: 3c643f62985ba914319d14d6490aae9e0fed29d5

--- source-files.js
+++ source-files.js
@@ -1,12 +1,14 @@
-var sourcesIndex = {};
-sourcesIndex["hashed"] = {"name":"","files":["benchmarking.rs","chain_spec.rs","lib.rs","rpc.rs","service.rs"]};
-sourcesIndex["hashed_parachain"] = {"name":"","dirs":[{"name":"chain_spec","files":["hashed.rs","md5.rs","mod.rs"]}],"files":["cli.rs","command.rs","main.rs","rpc.rs","service.rs"]};
-sourcesIndex["hashed_parachain_runtime"] = {"name":"","dirs":[{"name":"weights","files":["block_weights.rs","extrinsic_weights.rs","mod.rs","paritydb_weights.rs","rocksdb_weights.rs"]}],"files":["constants.rs","lib.rs","xcm_config.rs"]};
-sourcesIndex["hashed_runtime"] = {"name":"","files":["constants.rs","lib.rs"]};
-sourcesIndex["pallet_bitcoin_vaults"] = {"name":"","files":["functions.rs","lib.rs","types.rs"]};
-sourcesIndex["pallet_confidential_docs"] = {"name":"","files":["functions.rs","lib.rs","types.rs"]};
-sourcesIndex["pallet_fruniques"] = {"name":"","files":["functions.rs","lib.rs"]};
-sourcesIndex["pallet_gated_marketplace"] = {"name":"","files":["functions.rs","lib.rs","types.rs"]};
-sourcesIndex["pallet_rbac"] = {"name":"","files":["functions.rs","lib.rs","types.rs"]};
-sourcesIndex["pallet_template"] = {"name":"","files":["lib.rs"]};
+var sourcesIndex = JSON.parse('{\
+"hashed":["",[],["benchmarking.rs","chain_spec.rs","lib.rs","rpc.rs","service.rs"]],\
+"hashed_parachain":["",[["chain_spec",[],["hashed.rs","md5.rs","mod.rs"]]],["cli.rs","command.rs","main.rs","rpc.rs","service.rs"]],\
+"hashed_parachain_runtime":["",[["weights",[],["block_weights.rs","extrinsic_weights.rs","mod.rs","paritydb_weights.rs","rocksdb_weights.rs"]]],["constants.rs","lib.rs","xcm_config.rs"]],\
+"hashed_runtime":["",[],["constants.rs","lib.rs"]],\
+"pallet_bitcoin_vaults":["",[],["functions.rs","lib.rs","types.rs"]],\
+"pallet_confidential_docs":["",[],["functions.rs","lib.rs","types.rs"]],\
+"pallet_fruniques":["",[],["functions.rs","lib.rs"]],\
+"pallet_gated_marketplace":["",[],["functions.rs","lib.rs","types.rs"]],\
+"pallet_proxy":["",[],["functions.rs","lib.rs","types.rs"]],\
+"pallet_rbac":["",[],["functions.rs","lib.rs","types.rs"]],\
+"pallet_template":["",[],["lib.rs"]]\
+}');
 createSourceSidebar();
