6692c4d53c1701704a9b8774dfb0ee613de0a412
Erick Casanova <desneruda@gmail.com>
2022-11-09 14:42:38 -0600

    Release 126

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -106,7 +106,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
 	// This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
 	//   the compatible custom types.
-	spec_version: 125,
+	spec_version: 126,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
@@ -579,7 +579,7 @@ impl pallet_fund_admin::Config for Runtime {
 		EnsureRoot<AccountId>,
 		pallet_collective::EnsureProportionAtLeast<AccountId, CouncilCollective, 3, 5>,
 	>;
-	
+
 	type MaxDocuments = MaxDocuments;
 	type MaxProjectsPerUser = MaxProjectsPerUser;
 	type MaxUserPerProject = MaxUserPerProject;
