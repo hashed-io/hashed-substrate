2b25e70f2a16d23a15f8128868afeb160fd456a6
Sebastian Montero <sebastian.montero@gmail.com>
2022-12-01 10:09:44 -0600

    Developed hashed-collator manifests. Developed luhn collator manifests. Updated start-collator script to accept the chain parameters.

--- scripts/start_collator.sh
+++ scripts/start_collator.sh
@@ -1,36 +1,47 @@
 #!/usr/bin/env bash
 
+
+usage="./start-collator.sh --chain [hashed|luhn]"
+if [ $# -ne 2 ]; then
+    echo $usage
+    exit 1
+fi
+
+if [[ $1 != '--chain' || ($2 != 'hashed' && $2 != 'luhn') ]]; then
+    echo $usage
+    exit 1
+fi
+
 #cargo build --release
 
 collator_args=(
-    --collator 
-    --base-path ./collator-data/ 
-    --force-authoring 
-    --port 40333 
-    --ws-port 9946 
-    --ws-external 
-    --rpc-external 
-    --rpc-cors all 
+    --collator
+    --base-path ./collator-data/
+    --force-authoring
+    --port 40333
+    --ws-port 9946
+    --ws-external
+    --rpc-external
+    --rpc-cors all
     --rpc-methods unsafe
 )
 
 relay_args=(
-    --execution wasm 
-    --base-path ./relay-data/ 
-    --chain /var/www/hashed-substrate/resources/polkadot.json 
+    --execution wasm
+    --base-path ./relay-data/
+    --chain /var/www/hashed-substrate/resources/polkadot.json
     --port 30333
-    --ws-port 9944 
-    --ws-external 
-    --rpc-external 
-    --rpc-cors all 
-    --rpc-methods unsafe 
+    --ws-port 9944
+    --ws-external
+    --rpc-external
+    --rpc-cors all
+    --rpc-methods unsafe
     --wasm-execution Compiled
     --pruning 10000
 )
 
-chain_spec="--chain hashed"
 collator_args+=($chain_spec)
 
 #/target/release/hashed key insert --base-path ./collator-data $chain_spec --scheme sr25519 --suri "${MNEMO}" --key-type aura
-
-/var/www/hashed-substrate/target/release/hashed-parachain "${collator_args[@]}" -- "${relay_args[@]}"
+# echo "${collator_args[@]}" $@ -- "${relay_args[@]}"
+/var/www/hashed-substrate/target/release/hashed-parachain "${collator_args[@]}" $@ -- "${relay_args[@]}"
