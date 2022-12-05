db74f8fd4c082538421b72dff6d63c986d7a3d81
Erick Casanova <desneruda@gmail.com>
2022-12-05 17:23:40 -0600

    Weights with default info

--- scripts/start_benchmarking.sh
+++ scripts/start_benchmarking.sh
@@ -4,10 +4,10 @@ echo "*** Initializing becnhmarking"
 
 cargo build --package node-template --release --features runtime-benchmarks
 
-./target/release/hashed benchmark pallet \
+./target/release/hashed benchmark pallet \                                                                                                                                                               1  6.99  100%
 --chain dev \
 --pallet pallet_template \
 --extrinsic '*' \
 --steps 20 \
 --repeat 10 \
---output pallets/pallet-template/src/weights.rs
+--output pallets/template/src/weights.rs
