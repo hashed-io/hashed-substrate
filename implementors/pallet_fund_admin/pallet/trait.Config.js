ed47371eece5d0e5fd05428cfcbcda0bff065fb3
tlacloc <tlacloc@users.noreply.github.com>
2022-12-06 18:10:44 +0000

    deploy: a384bb5fe7ae1d449067c274a9a4523fe569f3b0

--- implementors/pallet_fund_admin/pallet/trait.Config.js
+++ implementors/pallet_fund_admin/pallet/trait.Config.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"hashed_runtime":[["impl <a class=\"trait\" href=\"pallet_fund_admin/pallet/trait.Config.html\" title=\"trait pallet_fund_admin::pallet::Config\">Config</a> for <a class=\"struct\" href=\"hashed_runtime/struct.Runtime.html\" title=\"struct hashed_runtime::Runtime\">Runtime</a>"]]
+"hashed_runtime":[["impl Config for <a class=\"struct\" href=\"hashed_runtime/struct.Runtime.html\" title=\"struct hashed_runtime::Runtime\">Runtime</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
