04b2ae3acbfbfcfa453da8163b0cc1c069e1b642
Sebastian Montero <sebastian.montero@gmail.com>
2022-12-06 22:27:22 -0500

    Updated start collator script to accept the base-path as parameter. Updated hashed-collator manifest to account for the start_collator script. Updated luhn-collator manifest to account for the start_collator script.

--- scripts/start_collator.sh
+++ scripts/start_collator.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 
-usage="./start-collator.sh --chain [hashed|luhn]"
+usage="./start-collator.sh [hashed|luhn] <base-data-path>"
 if [ $# -ne 2 ]; then
     echo $usage
     exit 1
 fi
 
-if [[ $1 != '--chain' || ($2 != 'hashed' && $2 != 'luhn') ]]; then
+if [[ ($1 != 'hashed' && $1 != 'luhn') ]]; then
     echo $usage
     exit 1
 fi
@@ -16,7 +16,7 @@ fi
 
 collator_args=(
     --collator
-    --base-path ./collator-data/
+    --base-path $2/collator-data/
     --force-authoring
     --port 40333
     --ws-port 9946
@@ -24,11 +24,12 @@ collator_args=(
     --rpc-external
     --rpc-cors all
     --rpc-methods unsafe
+    --chain $1
 )
 
 relay_args=(
     --execution wasm
-    --base-path ./relay-data/
+    --base-path $2/relay-data/
     --chain /var/www/hashed-substrate/resources/polkadot.json
     --port 30333
     --ws-port 9944
@@ -43,5 +44,5 @@ relay_args=(
 collator_args+=($chain_spec)
 
 #/target/release/hashed key insert --base-path ./collator-data $chain_spec --scheme sr25519 --suri "${MNEMO}" --key-type aura
-# echo "${collator_args[@]}" $@ -- "${relay_args[@]}"
-/var/www/hashed-substrate/target/release/hashed-parachain "${collator_args[@]}" $@ -- "${relay_args[@]}"
+# echo "${collator_args[@]}" -- "${relay_args[@]}"
+/var/www/hashed-substrate/target/release/hashed-parachain "${collator_args[@]}" -- "${relay_args[@]}"
