7559abc218f195dbf645c2f12de68e74f74dd201
tlacloc <tlacloc@users.noreply.github.com>
2023-01-13 15:18:40 +0000

    deploy: f87d0c7f7869407f7923c774ea2840595c7a0e71

--- implementors/core/fmt/trait.Display.js
+++ implementors/core/fmt/trait.Display.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"pallet_bitcoin_vaults":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"pallet_bitcoin_vaults/types/crypto/struct.Public.html\" title=\"struct pallet_bitcoin_vaults::types::crypto::Public\">Public</a>"]]
+"pallet_bitcoin_vaults":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"pallet_bitcoin_vaults/types/crypto/struct.Public.html\" title=\"struct pallet_bitcoin_vaults::types::crypto::Public\">Public</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
