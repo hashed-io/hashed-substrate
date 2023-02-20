31dc08545885813f169ffe58e78906c9eb222cd9
Sebastian Montero <sebastian.montero@gmail.com>
2023-02-20 17:22:55 -0600

    Added chaos-raw-spec and run-node script

---
+++ scripts/run-node.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# DEBUG BOOTNODES
+# export RUST_LOG="sub-libp2p=trace"
+
+usage="./run-node.sh <chainspec> <data-path>"
+if [ $# -ne 2 ]; then
+    echo $usage
+    exit 1
+fi
+
+node_args=(
+    --base-path $2
+    --ws-external
+    --rpc-external
+    --rpc-cors all
+    --rpc-methods unsafe
+    --validator
+    --node-key ${NODEKEY}
+    --chain $1
+)
+
+if [[ ! -z ${BOOTNODES} ]]; then
+    node_args+=(--bootnodes ${BOOTNODES})
+fi
+
+echo "Inserting keys..."
+
+# echo "${node_args[@]}"
+
+# echo "${MNEMO}"
+
+./target/release/hashed key insert --base-path $2 --chain $1 --scheme sr25519 --suri "${MNEMO}" --key-type aura
+
+./target/release/hashed key insert --base-path $2 --chain $1 --scheme ed25519 --suri "${MNEMO}" --key-type gran
+
+./target/release/hashed "${node_args[@]}"
