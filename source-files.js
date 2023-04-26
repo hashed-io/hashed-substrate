a00cbb948fb2a020459ec09051aac5c6df85ffa4
didiermis <didiermis@users.noreply.github.com>
2023-04-26 17:56:53 +0000

    deploy: c5e8762ba7d96f6f4c21f247dfc29dfeee0bf2a5

--- source-files.js
+++ source-files.js
@@ -7,7 +7,7 @@ var sourcesIndex = JSON.parse('{\
 "pallet_bitcoin_vaults":["",[],["functions.rs","lib.rs","types.rs"]],\
 "pallet_confidential_docs":["",[],["functions.rs","lib.rs","types.rs"]],\
 "pallet_fruniques":["",[],["functions.rs","lib.rs","types.rs"]],\
-"pallet_fund_admin":["",[],["functions.rs","lib.rs","types.rs"]],\
+"pallet_fund_admin":["",[],["functions.rs","lib.rs","migration.rs","types.rs"]],\
 "pallet_gated_marketplace":["",[],["functions.rs","lib.rs","types.rs"]],\
 "pallet_mapped_assets":["",[],["extra_mutator.rs","functions.rs","impl_fungibles.rs","impl_stored_map.rs","lib.rs","types.rs","weights.rs"]],\
 "pallet_rbac":["",[],["functions.rs","lib.rs","types.rs"]],\
