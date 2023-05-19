dc7a934401734cd34fca2b347510c745f858ac40
tlacloc <tlacloc@users.noreply.github.com>
2023-05-19 18:44:35 +0000

    deploy: 336967e6056e6fc00d7320d627c65a9ebf787e8d

--- implementors/pallet_gated_marketplace/pallet/trait.Config.js
+++ implementors/pallet_gated_marketplace/pallet/trait.Config.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"hashed_parachain_runtime":[["impl Config for <a class=\"struct\" href=\"hashed_parachain_runtime/struct.Runtime.html\" title=\"struct hashed_parachain_runtime::Runtime\">Runtime</a>"]],
+"hashed_parachain_runtime":[["impl <a class=\"trait\" href=\"pallet_gated_marketplace/pallet/trait.Config.html\" title=\"trait pallet_gated_marketplace::pallet::Config\">Config</a> for <a class=\"struct\" href=\"hashed_parachain_runtime/struct.Runtime.html\" title=\"struct hashed_parachain_runtime::Runtime\">Runtime</a>"]],
 "hashed_runtime":[["impl <a class=\"trait\" href=\"pallet_gated_marketplace/pallet/trait.Config.html\" title=\"trait pallet_gated_marketplace::pallet::Config\">Config</a> for <a class=\"struct\" href=\"hashed_runtime/struct.Runtime.html\" title=\"struct hashed_runtime::Runtime\">Runtime</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
